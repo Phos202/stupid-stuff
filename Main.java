557922903329cb91c50b4bf7091337075524f0e8
animeforever200323 <aedels65@haverfordsd.net>
2020-01-30 01:23:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    long x = 99999999;
+    String maths = "maths";
+    while (1==1) {
+      System.out.println(x + maths);
+      x += 9999999;}
+  }
+}
